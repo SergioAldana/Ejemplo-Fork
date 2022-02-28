ef091fa2d82d15f77b73d6b393e1dfbbea2a823a
SergioAldana <saaldana2@misena.edu.co>
2022-02-27 20:58:34 -0500

    chore: se modifica el main

--- main.c
+++ main.c
@@ -1,6 +1,27 @@
+#include <sys/types.h>
+#include <unistd.h>
 #include <stdio.h>
 
-int main() {
-    printf("Hello, World!\n");
+int main(int argc, char *argv[])
+{
+    pid_t pid1, pid2;
+
+    if ( (pid1=fork()) == 0 )
+    { /* hijo */
+        printf("Soy el primer hijo (%d, hijo de %d)\n",  getpid(), getppid());
+    }
+    else
+    { /*  padre */
+        if ( (pid2=fork()) == 0 )
+        { /* segundo hijo  */
+            printf("Soy el segundo hijo (%d, hijo de %d)\n",  getpid(), getppid());
+        }
+        else
+        {
+            printf("Soy el padre (%d, hijo de %d)\n", getpid(), getppid());
+            sleep(20);
+        }
+    }
+
     return 0;
 }
