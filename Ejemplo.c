6055f9c96c53936f65fa96eacc95579ac5075780
SergioAldana @SergioAldana <saaldana2@misena.edu.co>
2022-03-04 13:29:04 -0500

    chore: se modifican las funciones factorial y primo para que funcionen en C

--- Ejemplo.c
+++ Ejemplo.c
@@ -6,13 +6,13 @@ int main(int argc, char *argv[]) {
 
     pid_t pid1, pid2;
     int numero;
-    
+
     printf("Ingresa un numero: ");
     scanf("%d", &numero);
 
-    if ((pid1 = fork()) == 0) { 
-    
-    	// Hijo 
+    if ((pid1 = fork()) == 0) {
+
+        // Primer hijo
         int n1 = 0;
         int n2 = 1;
         int n3;
@@ -25,9 +25,30 @@ int main(int argc, char *argv[]) {
             n1 = n2;
             n2 = n3;
         }
-        
+    } else {
+
+        // Padre
+        if ((pid2 = fork()) == 0) {
+
+            // Segundo hijo
+            for (int i = 1; numero >= i; i++) {
+                if (numero % i == 0) {
+                    printf(" P: %d", i);
+                }
+            }
+        } else {
+
+            int factorial = 1;
+
+            for (int i = 1; numero >= i; i++) {
+                factorial = factorial * i;
+
+            }
+            printf(" Fc: %d", factorial);
+
+        }
     }
-    
+
     printf("\n");
 
     return 0;
